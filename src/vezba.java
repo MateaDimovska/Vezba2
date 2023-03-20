13d4fcf8c3144ff4f27d83b14c24250e6a75a02a
unknown <matea123dimovska@gmail.com>
2023-03-20 11:14:22 +0100

    Add files to repo

---
+++ src/vezba.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class vezba {
+
+	public static void main(String[] args) {
+		boolean p, q;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Input p:");
+		p = keyboard.nextBoolean();
+		System.out.println("Input q:");
+		q = keyboard.nextBoolean();
+		
+		if(p && q) {
+			q = false;
+		}else {
+			if(!q) {
+				System.out.println(p);
+			}
+			if(p == q) {
+				System.out.println(p || q);
+			}
+		}
+		
+		System.out.println(q);
+	}
+}
